8d9c644ec93c30f7f4e31d6ef676fecdea065d9a
Brent Royal-Gordon <brent@architechies.com>
2012-08-21 16:15:26 -0700

    Add methods to fetch an image

--- ANImage.h
+++ ANImage.h
@@ -8,16 +8,24 @@
 
 #import "ANResource.h"
 
-//typedef void (^ANImageCompletion)(UIImage * image, NSError * error);
+// ANFrameworkImage is the same as NSImage on the Mac or UIImage on the iPhone.
+// (It just so happens that ANImage only needs to call -initWithData: which is present in both.)
+#if defined(TARGET_OS_IPHONE) || defined(TARGET_IPHONE_SIMULATOR)
+@compatibility_alias ANFrameworkImage UIImage;
+#else
+@compatibility_alias ANFrameworkImage NSImage;
+#endif
+
+typedef void (^ANImageCompletion)(ANFrameworkImage * image, NSError * error);
 
 @interface ANImage : ANResource
 
 @property (readonly) NSURL * URL;
 @property (readonly) CGSize nativeSize;
 
-//- (void)imageWithCompletion:(ANImageCompletion)completion;
-//- (void)imageAtWidth:(CGFloat)width completion:(ANImageCompletion)completion;
-//- (void)imageAtHeight:(CGFloat)height completion:(ANImageCompletion)completion;
-//- (void)imageAtSize:(CGSize)size completion:(ANImageCompletion)completion;
+- (void)imageWithCompletion:(ANImageCompletion)completion;
+- (void)imageAtWidth:(CGFloat)width completion:(ANImageCompletion)completion;
+- (void)imageAtHeight:(CGFloat)height completion:(ANImageCompletion)completion;
+- (void)imageAtSize:(CGSize)size completion:(ANImageCompletion)completion;
 
 @end
