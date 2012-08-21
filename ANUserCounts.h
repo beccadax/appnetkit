5ff2d895d0018158747ea450c2fe943e02eaf347
Brent Royal-Gordon <brent@architechies.com>
2012-08-21 16:38:57 -0700

    Make ANUser.counts work

---
+++ ANUserCounts.h
@@ -0,0 +1,17 @@
+//
+//  ANUserCounts.h
+//  Appdate
+//
+//  Created by Brent Royal-Gordon on 8/21/12.
+//  Copyright (c) 2012 Architechies. All rights reserved.
+//
+
+#import "ANResource.h"
+
+@interface ANUserCounts : ANResource
+
+@property (readonly) NSUInteger follows;
+@property (readonly) NSUInteger followedBy;
+@property (readonly) NSUInteger posts;
+
+@end
