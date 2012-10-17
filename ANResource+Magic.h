dfcafc06fc02b59f6440328f7998623ef8047c35
Brent Royal-Gordon <brent@architechies.com>
2012-10-16 17:23:44 -0700

    Declare AppNetKit to be MIT-licensed

--- ANResource+Magic.h
+++ ANResource+Magic.h
@@ -3,7 +3,7 @@
 //  AppNetKit
 //
 //  Created by Brent Royal-Gordon on 8/22/12.
-//  Copyright (c) 2012 Architechies. All rights reserved.
+//  Copyright (c) 2012 Architechies. See README.md for licensing information.
 //
 
 #import "ANResource.h"
